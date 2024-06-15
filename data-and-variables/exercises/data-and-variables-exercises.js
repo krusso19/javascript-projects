c53d16550d0ddc6e3eb7794226d382dc46178795
Katie Russo <krusso196@gmail.com>
2024-06-14 22:37:38 -0400

    completed with days to Moon output message

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -17,17 +17,23 @@ console.log(typeof milesKm)
 
 console.log(distanceMarsKm*milesKm)
 let milesToMars = distanceMarsKm*milesKm
-console.log(milesToMars)
+console.log("miles to Mars - "+milesToMars) // using to track information because of wrong answer, fixed
 
 let hoursToMars = milesToMars/shuttleSpeedMph
 let daysToMars = hoursToMars/24
-
 
 // Print the results of the space mission calculations below
 
 console.log(shuttleName+' will take '+daysToMars+' days to reach Mars.')
 
-
 // Calculate a trip to the moon below
 
+let milesToMoon = distanceMoonKm*milesKm
+console.log("miles to moon - "+milesToMoon) // using to track information because of wrong answer, fixed
+let hoursToMoon = milesToMoon/shuttleSpeedMph
+let daysToMoon = hoursToMoon/24
+
+
 // Print the results of the trip to the moon below
+
+console.log(shuttleName+' will take '+daysToMoon+' days to reach the Moon.')
