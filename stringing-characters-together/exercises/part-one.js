b3d9b400265100439d21f2ec657064501ab3ee4d
Katie Russo <krusso196@gmail.com>
2024-06-24 15:25:00 -0400

    Chp 7 exercises part 1-3 completed

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,21 @@
-let num = 1001;
+let number1 = 123.45;
 
 //Returns 'undefined'.
-console.log(num.length);
+console.log(number1.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+console.log((String(number1)).length);
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+console.log((Number((String(number1)).length))-1)
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+console.log(String(number1))
+
+if (String(number1).includes(".")) {
+  console.log(String(number1).length-1);
+}
+
+else {
+    console.log(String(number1).length);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,21 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
+console.log(language.slice(0,1)+language.slice(4,5));
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
+//KT - I don't think this is method chaining as I don't think I'm using any methods or "." but I'm not sure how else to do this and there is no example/check your work."
+console.log(language[0] + language[4]);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+console.log(`The abbreviation for ${language} is ${language[0]+language[4]}.`);
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+console.log(language.replace("va","su").slice(2,5).toLowerCase());
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+console.log(notTitleCase[0].toUpperCase()+(notTitleCase.slice(1,6))+notTitleCase[6].toUpperCase()+(notTitleCase.slice(7,10)));
