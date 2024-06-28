4c8adb5c8ebde06ed9396a478a693618b6e6e37e
Katie Russo <krusso196@gmail.com>
2024-06-27 20:21:30 -0400

    loops practice

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
-
+let names = ["Katie", "Russo", "Anna", "Russo"]
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,8 @@
 // Create a string variable containing your name.
-
+let nameFirst = "Katie"
 
 // Write a for loop that prints each character in your name on a different line.
+
+for (let i=0; i<nameFirst.length; i++) {
+    console.log(nameFirst[i]);
+}
