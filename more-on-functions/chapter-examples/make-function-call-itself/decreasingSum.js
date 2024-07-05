032bc53e3e924d1fe7c932d1053b272928964cce
Katie Russo <krusso196@gmail.com>
2024-07-05 12:07:24 -0400

    Chapter 11 Practice Problems

--- more-on-functions/chapter-examples/make-function-call-itself/decreasingSum.js
+++ more-on-functions/chapter-examples/make-function-call-itself/decreasingSum.js
@@ -2,8 +2,8 @@ function decreasingSum(integer) {
    if (integer === 1){
       return integer;
    } else {
-      //call decreasingSum function again
+      return integer + (decreasingSum(integer-1));
    }
 }
 
-console.log(decreasingSum(5));
+console.log(decreasingSum(3));
--- more-on-functions/chapter-examples/make-function-call-itself/function-calls-itself.js
+++ more-on-functions/chapter-examples/make-function-call-itself/function-calls-itself.js
@@ -2,7 +2,7 @@ function combineEntries(arrayName) {
   if (arrayName.length <= 1){
     return arrayName[0];
   } else {
-    //console.log(arrayName[0], arrayName.slice(1));
+    console.log(arrayName[0], arrayName.slice(1));
     return arrayName[0] + combineEntries(arrayName.slice(1));
   }
 }
@@ -10,7 +10,7 @@ function combineEntries(arrayName) {
 //First, run the code to see the result.
 
 //Next, uncomment the console.log statement above to see how each call to combineEntries looks at a different section of the original array.
-
+//KT - why does the code concatenate the string at the end?
 let arr = ['L', 'C', '1', '0', '1'];
 
 console.log(combineEntries(arr));
