9b8c95879294df043b42234f7e6fb81bc3a0e20c
Katie Russo <krusso196@gmail.com>
2024-07-05 13:15:29 -0400

    Chapter 11 exercises

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,6 +5,16 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
+let trippleNum = function(argument) {
+    if (typeof(argument) === "string") {
+        return "ARRR!";
+    } else if (typeof(argument) === "number") {
+        return Number(argument)*3
+    } else {
+        return argument;
+    }
+}
+console.log(trippleNum(4));
 
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
@@ -13,3 +23,7 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+let modArr = arr.map(trippleNum);
+
+console.log(modArr);
+
--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -8,6 +8,18 @@ function checkFuel(level) {
   }
 }
 
+//KT - why would I put this anywhere? it prints the amount. I would want it to return the amount somewhere and the color here.
+let definetlyTheShuttleFuelLevel = function(n) {
+  if (checkFuel(n) === "green") {
+    return (n - 100001);
+  } else if (checkFuel(n) === "yellow") {
+    return (n - 50001);
+  } else {
+    return (n);
+  }
+}
+//console.log(definetlyTheShuttleFuelLevel(55000));
+
 function holdStatus(arr){
   if (arr.length < 7) {
     return `Spaces available: ${7-arr.length}.`;
@@ -28,6 +40,18 @@ console.log("Hold status: " + holdStatus(cargoHold));
  */
  
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
+/*
+let definetlyTheShuttleFuelLevel = function(n) {
+  if (checkFuel(n) === "green") {
+    return (n - 100001);
+  } else if (checkFuel(n) === "yellow") {
+    return (n - 50001);
+  } else {
+    return (n);
+  }
+}
+  */
+//console.log(definetlyTheShuttleFuelLevel(55000));
 
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
 
@@ -39,6 +63,14 @@ console.log("Hold status: " + holdStatus(cargoHold));
  */
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
+let holdStats = function([]) {
+    stolenGoods = (cargoHold.splice((cargoHold.indexOf("gold")),1,"metal"));
+    stolenGoods.push(cargoHold.splice((cargoHold.indexOf("meal kits")),1,"meel"));
+    return cargoHold;
+}
+
+console.log(holdStats(cargoHold));
+console.log(stolenGoods);
 
 //b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
 
@@ -50,7 +82,18 @@ console.log("Hold status: " + holdStatus(cargoHold));
  */
  
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
-	
+	let irs = function(fuelLevel,cargoHold){
+    return `Took ${definetlyTheShuttleFuelLevel(fuelLevel)}kg of fuel and ${stolenGoods[0]} and ${stolenGoods[1]} from the cargo hold. The remaining cargo hold is ${holdStats(cargoHold)}.`
+  }
+
+
 //b). Call your anonymous fuel and cargo functions from within irs.
 
 //c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
+
+console.log(irs(fuelLevel,cargoHold));
+
+//KT - not sure why I'm getting meel twice on the output
+//KT - I'm not sure how to do step 4, liberating the cargo
+// and collect a new array I can use in the template literal
+// or how to make it general so it just picks the best things for me
