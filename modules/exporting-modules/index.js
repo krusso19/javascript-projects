87c4248f3ca621f38b74422513862d4542fbfdc3
Katie Russo <krusso196@gmail.com>
2024-07-18 09:16:09 -0700

    Chapter 13 practice problems

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,23 @@
 // Import the modules exported from practiceExports.js below:
+/*const palindromeCheck = require('./practiceExports.js');
+
+console.log(typeof palindromeCheck);
+console.log(palindromeCheck('mam'));
+*/
+
+const practice = require('./practiceExports.js');
+
+console.log(typeof practice);
+console.log(practice);
+
+let arrayExample = ['hello', 'world', 123, 987, 'LC101']
+console.log(practice.isPalindrome('mpm'));
+console.log(practice.evenOrOdd(9));
+
+console.log(practice.randomArrayElement(arrayExample));
+
+console.log("if you want more than one random:")
+
+for (i=0; i<4; i++) {
+    console.log(practice.randomArrayElement(arrayExample));
+}
