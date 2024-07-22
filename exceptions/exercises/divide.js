67e75a72174e500f1c545e0aa96d4055e8a27f7c
Katie Russo <krusso196@gmail.com>
2024-07-22 16:26:14 -0700

    Chp 15-17 Exercises

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,18 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+let numerator = 0
+let denominator = 0
+function divide(numerator, denominator){
+    if (denominator === 0){
+        throw Error("Attempted to divide by zero.");
+    } else {
+        let result = numerator/denominator;
+    return result;
+    }
+};
+
+console.log(divide(8,0)); //KT - wow I can't believe I did that on the first try!
+//KT - this code doesn't execute all the way and that is correct!
+//KT - it throws an error, which is jarring, but what is supposed to happen :p
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,7 +1,12 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result; //KT - declared but not initialized so result is global and the error with runLab happens within the try/catch block
+    try {
+    result = lab.runLab(3);
+  } catch(err) {
+    console.log("Error thrown.");
+  }
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
@@ -22,3 +27,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
