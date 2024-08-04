1b04ed09a4e377340697200465e8625350c915f1
Katie Russo <krusso196@gmail.com>
2024-08-04 12:10:38 -0400

    cleaning up git status, old git push

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,27 @@
 // launchcode.test.js code:
+const { exec } = require('child_process');
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test("should contain the key value pair organization:'nonprofit'", function(){
+   let organization = launchcode.organization();
+    expect(organization).toBe("nonprofit");
+  });
 
+  test("should contain the key value pair executiveDirector: 'Jeff'", function(){
+    let executiveDirector = launchcode.executiveDirector();
+    expect(executiveDirector).toBe("Jeff");
+  });
+
+  test("should contain percentageCoolEmployees", function(){
+    let percentageCoolEmployees = launchcode.percentageCoolEmployees();
+    expect(percentageCoolEmployees).toBe(!undefined);
+  });
+
+  test("should", function(){
+    expect(launchcode()).toBe();
+  });
+  
   // Write your unit tests here!
   
 });
